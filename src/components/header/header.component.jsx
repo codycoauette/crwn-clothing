2aedabb7aba4effc9f95c8e8341fc2a0bc5c0b8b
codycoauette <codycoauette@yahoo.com>
2020-05-05 15:51:21 -0500

    created user and cart sagas for sign in, sign up and sign in with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selector';
+import {signOutStart} from '../../redux/user/user.actions';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -14,7 +14,7 @@ import {HeaderContainer, LogoContainer, OptionsContainer, OptionLink} from './he
 
 import './header.styles.scss';
 
-const Header = ({currentUser, hidden}) => (
+const Header = ({currentUser, hidden, signOutStart}) => (
     <HeaderContainer>
         <LogoContainer to="/">
             <Logo className='logo' />
@@ -27,7 +27,7 @@ const Header = ({currentUser, hidden}) => (
             CONTACT            
             </OptionLink>
             {
-                currentUser ? (<OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>)
+                currentUser ? (<OptionLink as='div' onClick={signOutStart}>SIGN OUT</OptionLink>)
                 :
                 (<OptionLink to='/signin'>SIGN IN</OptionLink>)
             }
@@ -42,4 +42,8 @@ const mapStateToProps = createStructuredSelector({
     hidden: selectCartHidden
 });
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = dispatch => ({
+    signOutStart: () => dispatch(signOutStart())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import {googleSignInStart, emailSignInStart} from '../../redux/user/user.actions';
 
 import './sign-in.styles.scss';
 
@@ -19,19 +20,12 @@ class SignIn extends React.Component{
 
     handleSubmit = async event => {
         event.preventDefault();
-
+        const {emailSignInStart} = this.props;
         const { email, password } = this.state;
 
-        try{
-            await auth.signInWithEmailAndPassword(email, password);
-            this.setState({ email: '', password: '' });
-        }catch(error)
-        {
-            console.log(error);
-        }
-
+        emailSignInStart(email, password);
         
-    }
+    };
 
     handleChange = event => {
         const { value, name } = event.target;
@@ -40,6 +34,7 @@ class SignIn extends React.Component{
     }
 
     render() {
+        const {googleSignInStart} = this.props;
         return(
         <div className='sign-in'>
             <h2>I already have an account</h2>
@@ -63,7 +58,7 @@ class SignIn extends React.Component{
                 />
                 <div className='buttons'>
                     <CustomButton type='submit'>Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In with Google</CustomButton>
+                    <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn>Sign In with Google</CustomButton>
                 </div>
             </form>
         </div>
@@ -71,4 +66,9 @@ class SignIn extends React.Component{
     }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart()),
+    emailSignInStart: (email, password) => dispatch(emailSignInStart({email, password}))
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
